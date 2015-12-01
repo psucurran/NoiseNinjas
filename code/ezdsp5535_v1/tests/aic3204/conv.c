b462f102304db26d702dccc45b94e7b66450a917
psucurran <spc5213@gmail.com>
2015-11-30 19:05:34 -0500

    Revert "speed measure 4"
    This reverts commit 2c721e7afa2dcca52b24912fc8236e0d5c21c8df.

--- code/ezdsp5535_v1/tests/aic3204/conv.c
+++ code/ezdsp5535_v1/tests/aic3204/conv.c
@@ -12,7 +12,7 @@ Int16 convq(Queue *q, Int16 *h)
 {
 	Int16 qindex;
 	Int16 i;
-	Int16 result = 0;
+	long result = 0;
 	
 	//go from most recent values to least recent
 	qindex = q->tail;
@@ -27,7 +27,7 @@ Int16 convq(Queue *q, Int16 *h)
 		else
 			qindex--;
 			
-		result = result + h[i]*(q->Q)[qindex];
+		_smaci(result,h[i],(q->Q)[qindex]);
 	}
 	return ((Int16) result);
 }
